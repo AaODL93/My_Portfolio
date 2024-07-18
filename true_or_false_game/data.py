b24c437a643b5dddc69abb5bbb18096bd4c0d6ee
Aarón De León <150086440+AaODL93@users.noreply.github.com>
2024-07-18 10:26:02 -0600

    Upload related files.
    Each file includes code for an specific part of the program.

---
+++ true_or_false_game/data.py
@@ -0,0 +1,74 @@
+question_data = [
+    {"category": "Entertainment: Video Games",
+     "type": "boolean",
+     "difficulty": "easy",
+     "question": "Peter Molyneux was the founder of Bullfrog Productions.",
+     "correct_answer": "True",
+     "incorrect_answers": ["False"]
+     },
+    {"category": "Entertainment: Video Games",
+     "type": "boolean",
+     "difficulty": "easy",
+     "question": "English new wave musician Gary Numan founded the "
+                 "video game development company Facepunch Studios in March 2009.",
+     "correct_answer": "False", "incorrect_answers": ["True"]
+     },
+    {"category": "Entertainment: Video Games",
+     "type": "boolean",
+     "difficulty": "easy",
+     "question": "The game &quot;Battlefield 1&quot; takes place during World War I.",
+     "correct_answer": "True",
+     "incorrect_answers": ["False"]
+     },
+    {"category": "Entertainment: Video Games",
+     "type": "boolean",
+     "difficulty": "easy",
+     "question": "Pac-Man was invented by the designer Toru Iwatani while he was eating pizza.",
+     "correct_answer": "True",
+     "incorrect_answers": ["False"]
+     },
+    {"category": "Entertainment: Video Games",
+     "type": "boolean",
+     "difficulty": "easy",
+     "question": "Tetris is the #1 best-selling video game of all-time.",
+     "correct_answer": "False",
+     "incorrect_answers": ["True"]
+     },
+    {"category": "Entertainment: Video Games",
+     "type": "boolean",
+     "difficulty": "easy",
+     "question": "Deus Ex (2000) does not feature the World Trade Center "
+                 "because it was destroyed by terrorist attacks according to the game&#039;s plot.",
+     "correct_answer": "True",
+     "incorrect_answers": ["False"]
+     },
+    {"category": "Entertainment: Video Games",
+     "type": "boolean",
+     "difficulty": "easy",
+     "question": "In the &quot;Half-Life&quot; series, &quot;H.E.V&quot; "
+                 "stands for &quot;Hazardous Evasiveness Vest&quot;",
+     "correct_answer": "False",
+     "incorrect_answers": ["True"]
+     },
+    {"category": "Entertainment: Video Games",
+     "type": "boolean",
+     "difficulty": "easy",
+     "question": "The end credits sequence in Grand Theft Auto 5 is over half an hour long.",
+     "correct_answer": "True",
+     "incorrect_answers": ["False"]
+     },
+    {"category": "Entertainment: Video Games",
+     "type": "boolean",
+     "difficulty": "easy",
+     "question": "In Resident Evil 4, the Chicago Typewriter has infinite ammo.",
+     "correct_answer": "True",
+     "incorrect_answers": ["False"]
+     },
+    {"category": "Entertainment: Video Games",
+     "type": "boolean",
+     "difficulty": "easy",
+     "question": "The shotgun appears in every numbered Resident Evil game.",
+     "correct_answer": "True",
+     "incorrect_answers": ["False"]
+     },
+]
